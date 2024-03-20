24581dc600e36f17bb11c17fec1b8725c9d438e9
impossible98 <christoleavenow@outlook.com>
2024-03-20 22:18:46 +0800

    feat: change port

--- server.go
+++ server.go
@@ -1,13 +1,34 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	// import built-in packages
+	"log"
+	"os"
+
+	// import third-party packages
+	"github.com/dotenv-org/godotenvvault"
+	"github.com/gin-gonic/gin"
+
+	// import local packages
+	"phoenix/global"
+)
 
 func main() {
+	port := global.PORT
+	// load .env file
+	err := godotenvvault.Load()
+	// control flow
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port = os.Getenv("PORT")
+
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run()
+	r.Run(":" + port)
 }
